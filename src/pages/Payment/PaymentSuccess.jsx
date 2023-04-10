c02d5d64b932a3cac0f1dab7d634a734db470ad5
zihao <18326685991@163.com>
2023-04-10 19:51:42 +1000

    other debug log

--- src/pages/Payment/PaymentSuccess.jsx
+++ src/pages/Payment/PaymentSuccess.jsx
@@ -23,6 +23,7 @@ export default function PaymentSuccess() {
       const resp = await axios.post(url, paymentInfo);
       console.log(`resp is: ${resp}`);
       console.log(`data is: ${resp.data.status}`);
+      console.log(resp.data);
       if (resp.data.status === "confirmed") {
         console.log(`start to clear interval`);
         clearInterval(interval);
